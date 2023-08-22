7d8cb7495099a8e6391009a0cb0cdf3a651932f3
Samuel Holanda <samuel112928@gmail.com>
2023-08-22 16:16:27 -0300

    primeiro commit que vai da certo

---
+++ src/Aplication.java
@@ -0,0 +1,13 @@
+
+public class Aplication {
+	
+	int nota1;
+	int nota2;
+	int nota3;
+	public Aplication(int nota1, int nota2, int nota3) {
+	this.nota1 = nota1;
+	this.nota2 = nota2;
+	this.nota3 = nota3;
+
+	}
+}
